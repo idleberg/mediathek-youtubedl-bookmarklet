d4d7966718ae85b79fb102cf38bcb762a3668172
idleberg <git@idleberg.com>
2020-12-01 00:47:29 +0100

    minify favicon

--- bin/build.mjs
+++ bin/build.mjs
@@ -6,30 +6,36 @@ import { minify as htmlMinify } from 'html-minifier-terser';
 import { promises as fs } from 'fs';
 import { render } from 'ejs';
 
+const htmlMinifyOptions = {
+    collapseWhitespace: true,
+    removeAttributeQuotes: true,
+    removeComments: true
+};
+
 (async () => {
     const bookmarkletFile = resolve('./src/bookmarklet.js');
     const htmlFile = resolve('./src/index.ejs');
     const faviconFile = resolve('./src/favicon.svg');
 
     const bookmarklet = (await fs.readFile(bookmarkletFile)).toString();
+    const favicon = (await fs.readFile(faviconFile)).toString();
     const html = (await fs.readFile(htmlFile)).toString();
 
     const { code } = (await minify(bookmarklet));
     const href = `javascript:(function()\{${encodeURI(code)}\})()`;
 
-    const output = await htmlMinify(render(html, {bookmarklet: href}), {
-        collapseWhitespace: true,
-        removeAttributeQuotes: true,
-        removeComments: true
-    });
+    const htmlOutput = await htmlMinify(render(html, {bookmarklet: href}), htmlMinifyOptions);
+    const faviconOutput = await htmlMinify(favicon, htmlMinifyOptions);
+
     const outFolder = './public';
-    const outFile = join(outFolder, 'index.html');
+    const faviconOutputFile = join(outFolder, 'favicon.svg');
+    const htmlOutputFile = join(outFolder, 'index.html');
 
     await fs.rm('./public', {
         force: true,
         recursive: true
     });
     await fs.mkdir('./public');
-    await fs.copyFile(faviconFile, './public/favicon.svg');
-    await fs.writeFile(outFile, output);
+    await fs.writeFile(faviconOutputFile, faviconOutput);
+    await fs.writeFile(htmlOutputFile, htmlOutput);
 })();
