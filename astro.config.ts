41422795c752e6f58ab55184f374daec23552ea3
Jan T. Sott <git@idleberg.com>
2025-09-07 01:05:49 +0200

    chore: update astro config

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 import bookmarklet from 'vite-plugin-bookmarklet';
 
 export default defineConfig({
-	site: 'https://idleberg.github.io/mediathek-youtubedl-bookmarklet/',
+	site: 'https://idleberg.github.io',
+	base: '/mediathek-youtubedl-bookmarklet/',
+	output: 'static',
 	vite: {
 		plugins: [bookmarklet(), tailwindcss()],
 	},
