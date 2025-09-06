1c52072ac3fa8fe24546a83694888984a0921a68
Jan T. Sott <git@idleberg.com>
2025-09-07 00:56:12 +0200

    chore: add site prop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import bookmarklet from 'vite-plugin-bookmarklet'
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://idleberg.github.io/mediathek-youtubedl-bookmarklet/',
 		vite: {
 				plugins: [bookmarklet(), tailwindcss()]
 		}
